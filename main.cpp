357929cb873f132e0d66bb24b787ce3e26b6ced4
Yuchuan <blemiade_qinchuan@sjtu.edu.cn>
2025-03-16 21:43:15 +0800

    feat: adjust to the input format.

--- main.cpp
+++ main.cpp
@@ -1,64 +1,80 @@
-#include "CheckOnProduct/Product.h"
 #include "MCInclude.h"
-#include "Transform/NBA.h"
-#include <utility>
-#include <vector>
 using namespace antlr4;
 
 int main() {
-  std::ifstream stream;
-  stream.open("input.txt");
-
-  ANTLRInputStream input(stream);
-  LTLLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
-  LTLParser parser(&tokens);
-
-  tree::ParseTree *tree = parser.formula();
-  std::cout << tree->toStringTree(&parser) << std::endl;
-
-  grammar::Bkornblume visitor;
-  visitor.visit(tree);
-  grammar::Node *output = std::any_cast<grammar::Node *>(visitor.visit(tree));
-  while (true) {
-    auto [new_output, changed] = grammar::Simplify(output);
-    if (changed) {
-      output = new_output;
-    } else {
-      break;
-    }
-  }
-  output->print();
-  std::set<std::string> ap;
-  std::set<grammar::Node*> closure;
-  std::set<unsigned long long> hashs;
-  grammar::Format(output, ap);
-  // for (const auto &i : ap) {
-  //   std::cout << i << std::endl;
-  // }
-  unsigned long long hash = output->hash();
+  int global_formula, partial_formula;
   grammar::TSParser ts_parser("ts.txt");
-  // ts_parser.Print();
-  // ap.clear();
-  std::cout << std::endl;
-  grammar::BuildClosure(output, closure, hashs);
-  std::vector<std::pair<grammar::Node *, grammar::Node*>> pairs;
-  auto element_set = grammar::GetElementSet(closure, pairs);
-  // std::cout << "Element set:" << std::endl;
-  // for (const auto &i : element_set) {
-  //   for (const auto &j : i) {
-  //     j->print();
-  //     std::cout << ", ";
-  //   }
-  //   std::cout << std::endl;
-  // }
-  grammar::GNBA gnba(element_set, output, pairs);
-  // gnba.print();
-  grammar::NBA nba(gnba);
-  // nba.print();
-  grammar::Product product(ts_parser, nba, output);
-  product.check_hold();
-  std::cout << product.Check() << std::endl;
-  stream.close();
+  std::fstream file("input.txt");
+  std::string res;
+  file >> global_formula >> partial_formula;
+  file.ignore();
+  for (int i = 0; i < global_formula; i++) {
+    std::string formula;
+    getline(file, formula);
+    ANTLRInputStream input(formula);
+    LTLLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+    LTLParser parser(&tokens);
+    tree::ParseTree *tree = parser.formula();
+    grammar::Bkornblume visitor;
+    visitor.visit(tree);
+    grammar::Node *output = std::any_cast<grammar::Node *>(visitor.visit(tree));
+    while (true) {
+      auto [new_output, changed] = grammar::Simplify(output);
+      if (changed) {
+        output = new_output;
+      } else {
+        break;
+      }
+    }
+    std::set<std::string> ap;
+    std::set<grammar::Node *> closure;
+    std::set<unsigned long long> hashs;
+    grammar::Format(output, ap);
+    unsigned long long hash = output->hash();
+    grammar::BuildClosure(output, closure, hashs);
+    std::vector<std::pair<grammar::Node *, grammar::Node *>> pairs;
+    auto element_set = grammar::GetElementSet(closure, pairs);
+    grammar::GNBA gnba(element_set, output, pairs);
+    grammar::NBA nba(gnba);
+    grammar::Product product(ts_parser, nba, output);
+    std::cout << product.Check() << std::endl;
+  }
+  for (int i = 0; i < partial_formula; i++) {
+    int state_id;
+    std::string formula;
+    file >> state_id;
+    file.ignore();
+    std::getline(file, formula);
+    ANTLRInputStream input(formula);
+    LTLLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+    LTLParser parser(&tokens);
+    tree::ParseTree *tree = parser.formula();
+    grammar::Bkornblume visitor;
+    visitor.visit(tree);
+    grammar::Node *output = std::any_cast<grammar::Node *>(visitor.visit(tree));
+    while (true) {
+      auto [new_output, changed] = grammar::Simplify(output);
+      if (changed) {
+        output = new_output;
+      } else {
+        break;
+      }
+    }
+    std::set<std::string> ap;
+    std::set<grammar::Node *> closure;
+    std::set<unsigned long long> hashs;
+    grammar::Format(output, ap);
+    unsigned long long hash = output->hash();
+    grammar::BuildClosure(output, closure, hashs);
+    std::vector<std::pair<grammar::Node *, grammar::Node *>> pairs;
+    auto element_set = grammar::GetElementSet(closure, pairs);
+    grammar::GNBA gnba(element_set, output, pairs);
+    grammar::NBA nba(gnba);
+    grammar::Product product(ts_parser, nba, output);
+    // std::cout << product.Check() << std::endl;
+  }
+  fclose(stdin);
   return 0;
 }
